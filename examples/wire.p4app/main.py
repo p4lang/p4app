cae035a93c448c70a812745dcb2ea08d4b1228a1
Theo <theojepsen@g-m-a-i-l-dotcom>
2018-09-07 16:54:18 +0200

    updated wire example

--- examples/wire.p4app/main.py
+++ examples/wire.p4app/main.py
@@ -1,17 +1,15 @@
 from p4app import P4Mininet
-from mininet.topolib import TreeTopo
+from mininet.topo import SingleSwitchTopo
 from mininet.cli import CLI
 
-topo = TreeTopo(depth=2, fanout=2)
-
+topo = SingleSwitchTopo(2)
 net = P4Mininet(program='wire.p4', topo=topo)
 net.start()
 
+#CLI(net)
+
 h1 = net.get('h1')
-proc = h1.popen('ping -c2 10.0.0.2'.split())
+proc = h1.popen('ping -c2 ' + net.get('h2').intfs[0].ip)
 stdout, stderr = proc.communicate()
 print stdout, stderr
 assert proc.returncode == 0
-
-
-#CLI(net)
